300069da3f5ed66110e7d1260feb69f7e18d853d
kemengjian <mengjian.ke@hand-china.com>
2018-09-28 16:29:50 +0800

    first commit

---
+++ src/main/java/com/kmj/krdbase/KrdbaseApplication.java
@@ -0,0 +1,12 @@
+package com.kmj.krdbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrdbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KrdbaseApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kmj/krdbase/KrdbaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kmj.krdbase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KrdbaseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
